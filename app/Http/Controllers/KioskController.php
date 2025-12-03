584dcb64519af1d5c99210989220139808bbec76
Michael Rosero <michael.rosero@akold.com>
2025-12-03 15:57:22 -0500

    Prod dorado

--- app/Http/Controllers/KioskController.php
+++ app/Http/Controllers/KioskController.php
@@ -190,8 +190,8 @@ class KioskController extends Controller
                 ->with('accessToken',$token)
                 ->with('mac',$mac);
         }else{
-            // return view('kiosk.lista-metodos-pago')
-            return view('kiosk.lista-metodos-pago-diferido')
+            // return view('kiosk.lista-metodos-pago-diferido')
+            return view('kiosk.lista-metodos-pago')
                     ->with('accessToken',$token)
                     ->with('mac',$mac);
         }
