b316163bd8bf8b9852881393751cb9198fbf94c9
Michael Rosero <michael.rosero@akold.com>
2026-01-19 10:18:30 -0500

    Contador

--- resources/views/kiosk/detalle-agenda-multiple.blade.php
+++ resources/views/kiosk/detalle-agenda-multiple.blade.php
@@ -26,11 +26,13 @@
 		let elem = ``;
 		if(dataCita.items.length == dataCita.detalle_multiple.length){
 			elem += `<p class="m-0 mb-3 text-royal-blue fs-40 line-height-48 fw-bold page-title">¡Listo!</p>
+				<p class="m-0 mb-3 text-royal-blue fs-32 line-height-40 fw-bold">${dataCita.detalle_pre_agendamiento.length} de ${dataCita.items.length}</p>
 				<p class="fs-32 line-height-40 fw-medium mb-3">Terapías agendadas</p>
 				<img src="{{ request()->getHost() === '127.0.0.1' ? url('/') : secure_url('/') }}/assets/images/terapia.svg" class="w-50 my-5" alt="">
 				<a href="/carrito/{{ $mac }}" id="btn-continuar" class="btn py-24 px-5 bg-royal-blue text-white rounded-12 fs-24 line-height-32 w-auto">Ir al carrito</a>`;
 		}else{
 			elem += `<p class="m-0 mb-3 text-royal-blue fs-40 line-height-48 fw-bold page-title">¡Listo!</p>
+				<p class="m-0 mb-3 text-royal-blue fs-40 line-height-48 fw-bold page-title">${dataCita.detalle_pre_agendamiento.length} de ${dataCita.items.length}</p>
 				<p class="fs-32 line-height-40 fw-medium mb-3">Elige tu siguiente cita</p>
 				<img src="{{ request()->getHost() === '127.0.0.1' ? url('/') : secure_url('/') }}/assets/images/terapia.svg" class="w-50 my-5" alt="">
 				<a href="/citas-elegir-fecha-doctor/{{ $mac }}" id="btn-continuar" class="btn py-24 px-5 bg-royal-blue text-white rounded-12 fs-24 line-height-32 w-auto">Continuar agendando</a>`;
